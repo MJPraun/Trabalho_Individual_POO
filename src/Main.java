e9debff0b5b1381f9ef7e71e03767299dc69b546
Mario Praun <mpraun.dev@gmail.com>
2026-04-14 11:55:37 -0300

    Criando o menu inicial do projeto

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner
+
+public class Main {
+    public static void main (String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        double saldo = 0;
+        int quantidadeSaques = 0;
+        int opcao;
+
+        do{
+            System.out.println("\n ****** MENU CAIXA ELETRONICO ******");
+            System.out.println("1 - Verificar Saldo");
+            System.out.println("2 - Deposite aqui seu Dinheiro");
+            System.out.println("3 - Retire aqui seu Dinheiro");
+            System.out.println("0 - Sair");
+            System.out.println("Escolha uma opção:  ");
+            opcao = scanner.nextInt();
+            switch (opcao) {
+
+            }
+        }
+
+    }
+}
